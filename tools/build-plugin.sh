589695dbcf37db2c4c6edd4c80dd92dc68bd91fa
Derek Kaser <dkaser@users.noreply.github.com>
2023-05-10 23:34:56 -0400

    update trunk plugin URL (#34)

--- tools/build-plugin.sh
+++ tools/build-plugin.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-jinja -d plugin/tailscale.json -D branch main plugin/tailscale.j2 > ../plugin/tailscale.plg
-jinja -d plugin/tailscale.json -D branch trunk plugin/tailscale.j2 > ../plugin/tailscale-trunk.plg
+FILENAME=tailscale; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch main plugin/tailscale.j2 > ../plugin/$FILENAME.plg
+FILENAME=tailscale-trunk; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch trunk plugin/tailscale.j2 > ../plugin/$FILENAME.plg
