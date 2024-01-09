bac0b449b79fd0bcdaa9ad4b905cf4fe655c85da
Derek Kaser <dkaser@users.noreply.github.com>
2024-01-09 00:10:37 -0500

    build: 2024.01.09 (#106)
    Signed-off-by: Derek Kaser <derek.kaser@gmail.com>

--- tools/build-plugin.sh
+++ tools/build-plugin.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 FILENAME=tailscale; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch main plugin/tailscale.j2 > ../plugin/$FILENAME.plg
-FILENAME=tailscale-trunk; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch trunk plugin/tailscale.j2 > ../plugin/$FILENAME.plg
+FILENAME=prerelease/tailscale; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch prerelease plugin/tailscale.j2 > ../plugin/$FILENAME.plg
+FILENAME=trunk/tailscale; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch trunk plugin/tailscale.j2 > ../plugin/$FILENAME.plg
