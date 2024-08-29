6cb6946b940c337089fbaaa6da4d360bc96fc9e2
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2024-08-29 00:37:21 +0000

    feat: add check for Unraid Connect flash backup

--- tools/plugin/files/install.sh
+++ tools/plugin/files/install.sh
@@ -48,6 +48,24 @@ rm -f $(ls /boot/config/plugins/{{ name }}/unraid-tailscale-utils-*.txz 2>/dev/n
 rm -f $(ls /boot/config/plugins/{{ name }}/unraid-plugin-diagnostics-*.txz 2>/dev/null | grep -v '{{ diagVersion }}')
 rm -f $(ls /boot/config/plugins/{{ name }}/*.tgz 2>/dev/null | grep -v '{{ tailscaleVersion }}')
 
+# check to see if the state file has been backed up to Unraid Connect
+if [ -d "/boot/.git" ]; then
+        if git --git-dir /boot/.git log --all --name-only --diff-filter=A -- config/plugins/tailscale/state/tailscaled.state | grep -q . ; then
+            echo "******************************"
+            echo "*          WARNING           *"
+            echo "******************************"
+            echo " "
+            echo "The Tailscale state file has been backed up to Unraid Connect via Flash backup." 
+            echo " "
+            echo "To remove this backup, please perform the following steps:"
+            echo "1. Go to Settings -> Management Access".
+            echo "2. Under Unraid Connect, deactivate flash backup. Select the option to also delete cloud backup."
+            echo "3. Reactivate flash backup."
+
+            /usr/local/emhttp/webGui/scripts/notify -l '/Settings/ManagementAccess' -i 'alert' -e 'Tailscale State' -s 'Tailscale state backed up to Unraid connect.' -d 'The Tailscale state file has been backed up to Unraid connect. This is a potential security risk. From the Management Settings page, deactivate flash backup and delete cloud backups, then reactivate flash backup.'
+        fi
+fi
+
 echo ""
 echo "----------------------------------------------------"
 echo " {{ name }} has been installed."
