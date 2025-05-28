135ff9b4b7c582498b316c28a0347681e531a0c5
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-05-28 04:08:32 +0000

    ci: simplify

--- tools/build-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-FILENAME=tailscale; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch main plugin/tailscale.j2 > ../plugin/$FILENAME.plg
-FILENAME=tailscale-preview; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch preview plugin/tailscale.j2 > ../plugin/$FILENAME.plg
-FILENAME=tailscale-trunk; jinja -d plugin/tailscale.json -D filename $FILENAME -D branch trunk plugin/tailscale.j2 > ../plugin/$FILENAME.plg
